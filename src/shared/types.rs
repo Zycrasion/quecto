5fc935a9de62ed994a42104e142c962314f1c26b
Zycrasion <hendrix.may@gmail.com>
2023-09-01 23:28:29 +1000

    MORE

--- src/shared/types.rs
+++ src/shared/types.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum QuectoType
 {
@@ -14,6 +16,36 @@ pub enum QuectoType
     Qbool,
     Qchar,
     Qstr
+}
+
+impl FromStr for QuectoType
+{
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> 
+    {
+        match s
+        {
+            "u8" =>  Ok(QuectoType::Qu8),
+            "u16" => Ok(QuectoType::Qu16),
+            "u32" => Ok(QuectoType::Qu32),
+            "u64" => Ok(QuectoType::Qu64),
+
+            "i8" =>  Ok(QuectoType::Qi8),
+            "i16" => Ok(QuectoType::Qi16),
+            "i32" => Ok(QuectoType::Qi32),
+            "i64" => Ok(QuectoType::Qi64),
+            
+            "f32" => Ok(QuectoType::Qf32),
+            "f64" => Ok(QuectoType::Qf64),
+
+            "bool" => Ok(QuectoType::Qbool),
+            "char" => Ok(QuectoType::Qchar),
+            "str" => Ok(QuectoType::Qstr),
+
+            _ => Err(())
+        }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
--- src/tokeniser/tokeniser.rs
+++ src/tokeniser/tokeniser.rs
@@ -55,7 +55,9 @@ impl Tokeniser
 #[cfg(test)]
 mod test
 {
-    use crate::tokeniser::token_types::QuectoToken;
+    use std::collections::HashMap;
+
+    use crate::{tokeniser::token_types::QuectoToken, shared::types::QuectoType};
 
     use super::Tokeniser;
 
@@ -85,5 +87,102 @@ mod test
         let subject = Tokeniser(subject.to_owned());
         let tokens = subject.tokenise();
         assert_eq!(QuectoToken::CharLiteral('c'), tokens[0])
+    }
+
+    #[test]
+    pub fn int_literal_test()
+    {
+        let subject = "10";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::IntLiteral(10), tokens[0])
+    }
+
+    #[test]
+    pub fn float_literal_test()
+    {
+        let subject = "10.5";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::FloatLiteral(10.5), tokens[0]);
+        let subject = "167.575";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::FloatLiteral(167.575), tokens[0])
+    }
+
+    #[test]
+    pub fn bool_literal_test()
+    {
+        let subject = "true";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::BoolLiteral(true), tokens[0]);
+        let subject = "false";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::BoolLiteral(false), tokens[0])
+    }
+
+    #[test]
+    pub fn identifier_test()
+    {
+        let subject = "variable_name";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::Identifier("variable_name".to_owned()), tokens[0])
+    }
+
+    #[test]
+    pub fn colon_semi_test()
+    {
+        let subject = ";:";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::SemiColon, tokens[0]);
+        assert_eq!(QuectoToken::Colon, tokens[1])
+    }
+
+    #[test]
+    pub fn punc_test()
+    {
+        let subject = ".(";
+        let subject = Tokeniser(subject.to_owned());
+        let tokens = subject.tokenise();
+        assert_eq!(QuectoToken::OtherPunctuation('.'), tokens[0]);
+        assert_eq!(QuectoToken::OtherPunctuation('('), tokens[1])
+    }
+
+    #[test]
+    pub fn type_test()
+    {
+        let mut map = HashMap::new();
+
+        map.insert("u8", QuectoType::Qu8);
+        map.insert("u16", QuectoType::Qu16);
+        map.insert("u32", QuectoType::Qu32);
+        map.insert("u64", QuectoType::Qu64);
+
+        map.insert("i8", QuectoType::Qi8);
+        map.insert("i16", QuectoType::Qi16);
+        map.insert("i32", QuectoType::Qi32);
+        map.insert("i64", QuectoType::Qi64);
+
+        map.insert("f32", QuectoType::Qf32);
+        map.insert("f64", QuectoType::Qf64);
+
+        map.insert("bool", QuectoType::Qbool);
+        map.insert("char", QuectoType::Qchar);
+        map.insert("str", QuectoType::Qstr);
+
+        let keys = map.keys().into_iter();
+
+        for k in keys
+        {
+            let subject = k.to_string();
+            let subject = Tokeniser(subject);
+            let tokens = subject.tokenise();
+            assert_eq!(QuectoToken::Type(map[k]), tokens[0]);
+        }
     }
 }
