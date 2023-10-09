f7a4756bc052847faf26b2b5d23edce26cdc4f08
Zycrasion <hendrix.may@gmail.com>
2023-10-09 20:52:44 +1100

    current method was unsustainable

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod compiler;
-pub mod parser;
-pub mod shared;
-pub mod tokeniser;
-pub mod assembly;
--- src/tokeniser/mod.rs
+++
@@ -1,4 +0,0 @@
-mod token_types;
-mod tokeniser;
-pub use token_types::*;
-pub use tokeniser::*;
